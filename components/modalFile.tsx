30a4035374842c3e842af261cc8b6440e79060f0
Martial2023 <martialhilarionavadra@gmail.com>
2025-01-09 14:23:16 +0100

    remove unused imports2

--- components/modalFile.tsx
+++ components/modalFile.tsx
@@ -95,8 +95,8 @@ const ModalFile = ({
 
             const formData = new FormData()
             formData.append("file", file as Blob)
-            //const response = await axios.post('http://127.0.0.1:8000/api/upload-csv/', formData, {
-            const response = await axios.post('https://382d-197-234-221-251.ngrok-free.app/api/upload-csv/', formData, {
+            const response = await axios.post('http://127.0.0.1:8000/api/upload-csv/', formData, {
+            //const response = await axios.post('https://382d-197-234-221-251.ngrok-free.app/api/upload-csv/', formData, {
                 headers: {
                     "Content-Type": "multipart/form-data",
                 }
